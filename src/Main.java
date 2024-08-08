d612249c5333c3da4f753b168045d4d196f0eae2
OliaPetrova <skopindom8@gmail.com>
2024-08-08 19:39:56 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -14,27 +14,27 @@ public class Main {
         long startTs = System.currentTimeMillis(); // start time
 
         for (String text : texts) {
-            Thread thread = new Thread (() ->{
-            int maxSize = 0;
-            for (int i = 0; i < text.length(); i++) {
-                for (int j = 0; j < text.length(); j++) {
-                    if (i >= j) {
-                        continue;
-                    }
-                    boolean bFound = false;
-                    for (int k = i; k < j; k++) {
-                        if (text.charAt(k) == 'b') {
-                            bFound = true;
-                            break;
+            Thread thread = new Thread(() -> {
+                int maxSize = 0;
+                for (int i = 0; i < text.length(); i++) {
+                    for (int j = 0; j < text.length(); j++) {
+                        if (i >= j) {
+                            continue;
+                        }
+                        boolean bFound = false;
+                        for (int k = i; k < j; k++) {
+                            if (text.charAt(k) == 'b') {
+                                bFound = true;
+                                break;
+                            }
+                        }
+                        if (!bFound && maxSize < j - i) {
+                            maxSize = j - i;
                         }
                     }
-                    if (!bFound && maxSize < j - i) {
-                        maxSize = j - i;
-                    }
                 }
-            }
-            System.out.println(text.substring(0, 100) + " -> " + maxSize);
-        });
+                System.out.println(text.substring(0, 100) + " -> " + maxSize);
+            });
             threads.add(thread);
             thread.start();
         }
